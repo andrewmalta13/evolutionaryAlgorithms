84f2b02bcd8e92cb90b787c77a51bfd5e8b6433a
Andrew Malta <andrew.malta@yale.edu>
2018-04-16 12:00:58 -0400

    add estools's reward agumentation hack for bipedal walker hardcore

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def simulate(env, model_cls, weights, num_trials, seed=None, render=False):
+
+def simulate(env, model_cls, weights, num_trials, bipedal_hack=False, seed=None, render=False):
     m = model_cls(weights)
     rewards = []
     for _ in xrange(num_trials):
@@ -8,12 +9,26 @@ def simulate(env, model_cls, weights, num_trials, seed=None, render=False):
         done = False
         best_reward = -float("inf")
         total = 0
+        stumbled = False  # hack from estool for bipedal hardcore
+        re
 
         while not done:
             if render:
                 env.render()
             action = m.forward(observation)
             observation, reward, done, info = env.step(action)
+
+            # hack for bipedhard's reward augmentation during training.
+            # stop the stumble from penalizing the agent too much
+            if bipedal_hack and reward == -100:
+                reward = 0
+                stumbled = True
+
+            # reward agents for completing the task and not stumbling
+            if done and bipedal_hack and (not stumbled) and (total_reward > 300):
+              total += 100
+            break
+
             total += reward
 
         rewards.append(total)
